f7323c9783cef0be8ca599121ba13aedd31ae686
Nisha Salim <nocturne.noire@gmail.com>
2017-04-14 14:08:08 +0530

    Use resource generator for Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214104018) do
+ActiveRecord::Schema.define(version: 20170414082207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
     t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.date     "date"
+    t.text     "rationale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
