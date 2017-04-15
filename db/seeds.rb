ae343b41eb0d039edf12d3da17fdfb0af337681d
Nisha Salim <nocturne.noire@gmail.com>
2017-04-16 00:30:04 +0530

    Implement creation process for posts and refactor for best practices

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@
 end
 
 puts "100 posts created"
+
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'navigate' do
+
   describe 'index' do 
   	it 'can be reached successfully' do
   	  visit posts_path
@@ -12,4 +13,22 @@ describe 'navigate' do
   	  expect(page).to have_content(/Posts/)
   	end
   end
+
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+
+    it 'can reach the new form successfully' do  
+      expect(page.status_code).to eq (200)
+    end
+
+    it 'can be created from the new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+      expect(page).to have_content("Some rationale")
+    end
+  end
+
 end
